dbd4a5cef9fdb58462c9bcd21aef0c072518fcad
Anum Afzal <anumafzaldogar@gmail.com>
2019-05-08 23:48:53 +0200

    Initial class for dataloader ++

--- Assignment2/pytorch_dataloader.py
+++ Assignment2/pytorch_dataloader.py
@@ -1,10 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: anum
+"""
+import os, sys
 import torch
-import pandas as pd
+#import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
-PATH = '/media/narvis/902310aa-b57d-49a2-841d-8199ff125fca/ATLAS/Registration/Data/Everything'
+PATH_TESTING = '/media/narvis/902310aa-b57d-49a2-841d-8199ff125fca/ATLAS/Registration/Data/Everything/Testing/ABD_LYMPH_004'
+PATH_TRAINING = '/media/narvis/902310aa-b57d-49a2-841d-8199ff125fca/ATLAS/Registration/Data/Everything/Training/'
+PATH_VALIDATION = '/media/narvis/902310aa-b57d-49a2-841d-8199ff125fca/ATLAS/Registration/Data/Everything/Validation/ABD_LYMPH_080/'
 
 
+def read_data():
+
+    # The directory for testing data is empty so we are not gonna do anything with it
+    #dirs_testing = os.listdir(PATH_TESTING)
+
+    dirs_training = []
+    dirs_temp = os.listdir(PATH_TRAINING)
+    for d in dirs_temp:
+        dirs_training += list(os.listdir(d))
+
+    print (dirs_training.shape)
+
+    dirs_validation = os.listdir(PATH_VALIDATION)
+    print(dirs_validation.shape)
+
+    return dirs_training, dirs_validation
+
+
+'''
 class XrayLandmarksDataset(Dataset):
 
     def __init__(self, label_file_path, image_file_path):
@@ -24,6 +51,7 @@ class XrayLandmarksDataset(Dataset):
     def transform(self):
         pass
 
+'''
+dirs_training, dirs_validation = read_data()
 
-
-test_dataloader = XrayLandmarksDataset(label_file_path='data/label.txt', image_file_path = 'data/xray.png' )
+#test_dataloader = XrayLandmarksDataset(label_file_path='data/label.txt', image_file_path = 'data/xray.png' )
