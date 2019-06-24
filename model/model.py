90cfbf64059c1b419e3367ad8e6d253962a3fc6d
GNRam <gnrameshwara@gmail.com>
2019-06-24 07:45:31 +0530

    Implementation of depthwise separable convolution in CPM

--- model/model.py
+++ model/model.py
@@ -8,15 +8,32 @@ def same_padding(kernel_size):
     return (kernel_size - 1) // 2
 
 
+class DepthwiseSeparableConvolution(BaseModel):
+    def __init__(self, in_channels, out_channels, kernel_size, padding=0):
+        super(DepthwiseSeparableConvolution, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=padding)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+
+        x = self.depthwise(x)
+        x = self.relu(x)
+
+        x = self.pointwise(x)
+        x = self.relu(x)
+
+        return x
+
 class X(BaseModel):
     def __init__(self, x_channels=128):
         super(X, self).__init__()
 
         self.convs = nn.ModuleList([
-            nn.Conv2d(1, x_channels, kernel_size=9, padding=same_padding(9)),
-            nn.Conv2d(x_channels, x_channels, kernel_size=9, padding=same_padding(9)),
-            nn.Conv2d(x_channels, x_channels, kernel_size=9, padding=same_padding(9)),
-            nn.Conv2d(x_channels, 32, kernel_size=5, padding=same_padding(5))
+            DepthwiseSeparableConvolution(in_channels=1, out_channels=x_channels, kernel_size=9, padding=same_padding(9)),
+            DepthwiseSeparableConvolution(in_channels=x_channels, out_channels=x_channels, kernel_size=9, padding=same_padding(9)),
+            DepthwiseSeparableConvolution(in_channels=x_channels, out_channels=x_channels, kernel_size=9, padding=same_padding(9)),
+            DepthwiseSeparableConvolution(in_channels=x_channels, out_channels=32, kernel_size=5, padding=same_padding(5)),
         ])
 
         self.max_pool = nn.MaxPool2d(3, 2, 1)
@@ -36,10 +53,11 @@ class Stage1(BaseModel):
     def __init__(self, x_channels=128, stage_channels=512, num_classes=23):
         super(Stage1, self).__init__()
         self.X = X(x_channels)
+
         self.convs = nn.ModuleList([
-            nn.Conv2d(32, stage_channels, kernel_size=9, padding=same_padding(9)),
-            nn.Conv2d(stage_channels, stage_channels, kernel_size=1),
-            nn.Conv2d(stage_channels, num_classes, kernel_size=1)
+            DepthwiseSeparableConvolution(in_channels=32, out_channels=stage_channels, kernel_size=9, padding=same_padding(9)),
+            DepthwiseSeparableConvolution(in_channels=stage_channels, out_channels=stage_channels, kernel_size=1),
+            DepthwiseSeparableConvolution(in_channels=stage_channels, out_channels=num_classes, kernel_size=1),
         ])
 
         self.max_pool = nn.MaxPool2d(3, 2, same_padding(3))
@@ -59,12 +77,13 @@ class StageN(BaseModel):
     def __init__(self, x_channels=128, num_classes=23):
         super(StageN, self).__init__()
         self.X = X(x_channels)
+
         self.convs = nn.ModuleList([
-            nn.Conv2d(32 + num_classes, x_channels, kernel_size=11, padding=same_padding(11)),
-            nn.Conv2d(x_channels, x_channels, kernel_size=11, padding=same_padding(11)),
-            nn.Conv2d(x_channels, x_channels, kernel_size=11, padding=same_padding(11)),
-            nn.Conv2d(x_channels, x_channels, kernel_size=1),
-            nn.Conv2d(x_channels, num_classes, kernel_size=1),
+            DepthwiseSeparableConvolution(in_channels=32 + num_classes, out_channels=x_channels, kernel_size=11, padding=same_padding(11)),
+            DepthwiseSeparableConvolution(in_channels=x_channels, out_channels=x_channels, kernel_size=11, padding=same_padding(11)),
+            DepthwiseSeparableConvolution(in_channels=x_channels, out_channels=x_channels, kernel_size=11, padding=same_padding(11)),
+            DepthwiseSeparableConvolution(in_channels=x_channels, out_channels=x_channels, kernel_size=1),
+            DepthwiseSeparableConvolution(in_channels=x_channels, out_channels=num_classes, kernel_size=1),
         ])
 
         self.max_pool = nn.MaxPool2d(3, 2, same_padding(3))
