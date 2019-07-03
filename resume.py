e8d6a6830f9a73a0ebabedb553df5e36f47faf67
Felix Schiegg <ga53wep@tum.de>
2019-07-03 04:43:23 +0200

    Adapted jupyter notebook to support resuming training with correct sigma

--- resume.py
+++ resume.py
@@ -27,7 +27,7 @@ def resume(run_dir=None, model_pth=None):
     
     config = SourceFileLoader("CONFIG", os.path.join(run_path, 'config.py')).load_module().CONFIG
     epoch = int(model_path.split('checkpoint-epoch')[-1][:-4])
-    config['data_loader']['args']['custom_args']['sigma'] = config['data_loader']['args']['custom_args']['sigma'] * config['data_loader']['args']['custom_args']['sigma_reduction_factor']   ** epoch
+    config['data_loader']['args']['custom_args']['sigma'] *= config['data_loader']['args']['custom_args']['sigma_reduction_factor']   ** epoch
 
     main(ConfigParser(config, model_path))
 
